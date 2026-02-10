89b17cf5f74218ca851c0b39ccee446fc7789623
rovertronic <rovertentertainment@gmail.com>
2026-02-10 16:15:21 -0600

    mario's modules has lore now lol

---
+++ src/game/lore.inc.c
@@ -0,0 +1,149 @@
+char gLoreEntry1[] = "10-2-2257\n\
+Entry 0x5E\n\
+Dr.Westein\n\
+\n\
+I have been experimenting with the synthesis of natural and artificial polymers. \
+When an electical impulse is applied to this new structure, it elicits various behaviors. \
+If I figure out how to find a consistent way of producing specific structures, this \
+could be a revolutionary new field of science.";
+
+char gLoreEntry2[] = "14-2-2257\n\
+Entry 0x5F\n\
+Dr.Westein\n\
+\n\
+After days of banging my head and getting nowhere, I finally got... something. I have \
+find a repeatable process... for one structure. It's not very useful either. At first, \
+I had thought it was a completely useless structure. I applied an electric impulse, and \
+nothing happened. However, half an hour later, my instruments registered an exact reading \
+of the initial impulse. So, there's that.";
+
+char gLoreEntry3[] = "15-2-2257\n\
+Entry 0x61\n\
+Dr.Westein\n\
+\n\
+MIPS Laboratories is running out of money again. Mark Henzy has submitted another request for \
+funding to the Mushroom Kingdom Science Commission. We're on thin ice, and things could go sour \
+for us if our funding gets denied.";
+
+char gLoreEntry4[] = "16-2-2257\n\
+Entry 0x60\n\
+Dr.Westein\n\
+\n\
+I spilled the hybrid polymer solution onto my arm. To my absolute surprise and dismay, a \
+significant delay was induced when I had the impulse to move my hand. Then it hit me- the \
+polymer solution reformed my DNA with the new solution. The implications of this discovery \
+is both horrifying and enamouring. This hybrid polymer won't only be useful for engineering, \
+but medical science as well. Maybe this will be our key to saving MIPS laboratories.";
+
+char gLoreEntry5[] = "17-2-2257\n\
+Entry 0x61\n\
+Dr.Westein\n\
+\n\
+My collegue Jon managed to synthesize another hybrid polymer today- an incredibly useful one at that, too. \
+This structure emits photons at a consistent frequency of 410 THZ. A brilliant red light. If we tweak our process, \
+we could potentially see some more colors... Maybe even radiation outside the visible color spectrum. We could blow \
+the microwave industry to smithereens... but I'm getting ahead of myself here.";
+
+char gLoreEntry6[] = "19-2-2257\n\
+Entry 0x62\n\
+Dr.Westein\n\
+\n\
+No progress so far... However, we have been able to do practical experiments with our 2 current \
+hybrid polymers. If you line up poly1 and poly2 back to back, it has a wave-like effect when an \
+electrical impulse is applied. We made a funny red flashing sign for MIPs laboratories. I also \
+poured some of 2 on the tip of my index finger. Now it glows. And it made me chuckle when I wagged \
+my finger around and it did a little light show. But now my finger is red... and I can't change it \
+back... oops.\n\
+At the beginning of March, the Mushroom Kingdom Science Comission holds a showfair for the public \
+to see the latest innovations of the scientific community. This will be a great opportunity to show \
+off our new hybrid polymer.";
+
+char gLoreEntry7[] = "21-2-2257\n\
+Entry 0x63\n\
+Dr.Westein\n\
+\n\
+Our funding was denied. If we don't attract investors at the MKSC showfair, this may be the end for MIPs \
+laboratories.";
+
+char gLoreEntry8[] = "28-2-2257\n\
+Entry 0x64\n\
+Dr.Westein\n\
+\n\
+We've been working tirelessly on a presentable technology to demonstrate our hybrid polymer. Jon, with his usual brilliance, \
+has created a functioning prototype for a low energy replacement for an LED screen. It can display text and basic images. \
+We were never able to get poly2 to emit different colors, so it's unfortunately still pretty primitive. We jerryrigged \
+our monitor to plug in with an HDMI cable, and convert pixels using a threshold to determine if each cell emits light or not.";
+
+char gLoreEntry10[] = "1-3-2257\n\
+Entry 0x66\n\
+Dr.Westein\n\
+\n\
+While browsing the MKSC showfair, I picked up an interesting book from a vendor. ''Beyond the Cursed Mirror''. Flipping thorugh \
+the pages, it definitely looks like an interesting piece of fiction. Fairly over the top and eccentric, but I'm \
+sure it'll be a good read.";
+
+char gLoreEntry11[] = "1-3-2257\n\
+Entry 0x67\n\
+Dr.Westein\n\
+\n\
+Our presentation was a failure of grand proportions. Nobody was interested in our hybrid polymer. Most were confused, but those who \
+looked into our presentation further just thought our discovery was just a sidegrade to an already existing technology \
+that's been perfected for hundreds of years. Which, to be fair, it pretty much is. There was a small \
+part of me hoping my innovation would be my big breakthrough. But, that's just dreamer thinking. Most people don't make it, \
+and that includes us.";
+
+char gLoreEntry12[] = "4-3-2257\n\
+Entry 0x68\n\
+Dr.Westein\n\
+\n\
+We have about a month left of funds before we close down the labs. I haven't really had the motivation \
+to work on anything, so I've just been reading through Beyond the Cursed Mirror. The narrative has \
+be captivated, and my eyes glued to the pages. It's a good getaway from my present situation.";
+
+char gLoreEntry13[] = "5-3-2257\n\
+Entry 0x69\n\
+Dr.Westein\n\
+\n\
+Despite our downtrodden state, we managed to create poly3 after some desparate tweaking. This won't help us under any circumstance, but I might \
+as well document the discovery. It exerts a random force when an electrical impulse is applied. \
+I sprained my wrist from a violent jerk reaction when I spilled the solution on my hand. It doesn't help that my left arm \
+is still suffering from a one second delay, despite me somewhat getting used to it.";
+
+char gLoreEntry14[] = "6-3-2257\n\
+Entry 0x6A\n\
+Dr.Westein\n\
+\n\
+I think I've had a fair spark of inspiration when reading BtCM. The Parasite Moon feeds and propels its \
+wicked cause via attention. Observation is critical to the interworkings of this terrible entity. \
+It's so integral to its operation that it evolved an entire process of siphoning attention from a \
+captive audience to fuel it's continued existence. Photons change their behavior when observed. Is\
+it possible that observation is interfering with the forces exerted by poly3?";
+
+char gLoreEntry15[] = "8-3-2257\n\
+Entry 0x6B\n\
+Dr.Westein\n\
+\n\
+This may be the breakthrough we were looking for to save MIPs labs. When we synthesize our hybrid \
+polymer 3 with star dust, the random force vector suddenly suspends and becomes a reliable force. \
+We can use some basic instruments to calibrate polymer3 to give us any force we want. We even have \
+a variant of poly 3 that essentially inverts any force vector applied to it.";
+
+char gLoreEntry16[] = "12-3-2257\n\
+Entry 0x6C\n\
+Dr.Westein\n\
+\n\
+We've been slowed down in our works due to wasting time delicately welding the polymers together. \
+Jon, in his infinite wisdom and knowledge, presented us with an elegant and simple solution: Interlocking Bricks. \
+I was baffled at the simplicity of the solution, yet how effective it was. We could join different \
+configurations of hybrid polymer together and have a multitude of different outcomes- almost as if \
+we were writing code with toy pieces.";
+
+char gLoreEntry17[] = "13-3-2257\n\
+Entry 0x6D\n\
+Dr.Westein\n\
+\n\
+I was reading the news paper today, and stumbled across an interesting article. Allegedly, there's a \
+cult forming around the BtCM book. No, not like a fandom- a literal cult. The article refers to them as \
+the 'Lunatic Imitators.' They believe that BtCM was written, not as fiction, but rather as a prophecy for \
+what is to come. Of course, they're attempts at replicating events in the book have been rather feeble so far, \
+so it's a silly thing to put any amount of thought towards.";
