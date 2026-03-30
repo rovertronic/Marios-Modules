1485bbbe2f73cc44564442acb2d45f7cea6b007c
RRovert <rovertentertainment@gmail.com>
2026-03-29 19:15:47 -0500

    king zogomb shuts his mouth

--- src/game/behaviors/king_bobomb.inc.c
+++ src/game/behaviors/king_bobomb.inc.c
@@ -38,8 +38,11 @@ void king_bobomb_act_inactive(void) { // act 0
             o->oSubAction++;
             seq_player_lower_volume(SEQ_MM64_BOSS, 60, 40);
         }
+    /*
     } else if (cur_obj_update_dialog_with_cutscene(MARIO_DIALOG_LOOK_UP,
         DIALOG_FLAG_TURN_TO_MARIO, CUTSCENE_DIALOG, NEWTEXT_BOSS_1)) {
+    */
+    } else {
         o->oAction = KING_BOBOMB_ACT_ACTIVE;
         o->oFlags |= OBJ_FLAG_HOLDABLE;
 
@@ -207,8 +210,11 @@ void king_bobomb_act_death(void) { // act 7
     }
 
     cur_obj_init_animation_with_sound(KING_BOBOMB_ANIM_HIT_GROUND);
+    /*
     if (cur_obj_update_dialog_with_cutscene(MARIO_DIALOG_LOOK_UP,
         DIALOG_FLAG_TEXT_DEFAULT, CUTSCENE_DIALOG, deathtext)) {
+    */
+    if (1) {
         create_sound_spawner(SOUND_OBJ_KING_WHOMP_DEATH);
 
         cur_obj_hide();
@@ -324,10 +330,10 @@ void king_bobomb_act_return_home(void) { // act 5
             break;
 
         case KING_BOBOMB_SUB_ACT_RETURN_HOME_DIALOG:
-            if (cur_obj_update_dialog_with_cutscene(MARIO_DIALOG_LOOK_UP,
-                DIALOG_FLAG_TURN_TO_MARIO, CUTSCENE_DIALOG, DIALOG_128)) {
+            //if (cur_obj_update_dialog_with_cutscene(MARIO_DIALOG_LOOK_UP,
+            //    DIALOG_FLAG_TURN_TO_MARIO, CUTSCENE_DIALOG, DIALOG_128)) {
                 o->oAction = KING_BOBOMB_ACT_ACTIVE;
-            }
+            //}
             break;
     }
 }
